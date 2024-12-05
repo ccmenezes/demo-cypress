3e5b9c68afa9237b2d35929a9828b847696fc3d2
ccmenezes <claudiacm.ti@gmail.com>
2024-12-05 20:14:39 +0000

    Update tests to the new app, remove husk, run prettier

--- cypress/e2e/saucedemo.cy.js
+++ cypress/e2e/saucedemo.cy.js
@@ -3,29 +3,22 @@ const data = require('../fixtures/data.json')
 const app = require('../fixtures/app.json')
 
 describe('Test login Saucedemo page', () => {
-    beforeEach( ()=> {
+    beforeEach(() => {
         cy.log('-> Go to web page')
         cy.visit(Cypress.env('main_page'))
         cy.log('->Validate the title page')
         cy.title().should('include', app.titleLoginPage)
     })
 
-    // it('Sucessfully login with standard user', ()=> {
-    //     cy.log('-> Type username')
-    //     cy.get('[data-test=username]').type(data.standard_user)
-    //     cy.log('-> Type the password')
-    //     cy.get('[data-test=password]').type(data.password)
-    //     cy.log('-> Click on login button')
-    //     cy.get('[data-test=login-button]').click()
-    //     cy.url().should('include', '/inventory.html')
-    // })
+    it('Should show error message for error user' , ()=> {
+        cy.loginPage(data.error_user, data.error_password)
+        cy.log('-> Throw an error message.')
+        cy.get('div.error-message-container').should('eq', app.loginErrorMessage)
+    })
 
-    it('Sucessfully login', ()=> {
-        console.log(data.standard_user, data.password);
-        
+    it('Should sucessfully login with correct credentials', () => {
         cy.loginPage(data.standard_user, data.password)
         cy.log('-> Validate afer success login be redirect to products page.')
         cy.url().should('include', '/inventory.html')
     })
-
 })
--- cypress/support/commands/saucedemo.js
+++ cypress/support/commands/saucedemo.js
@@ -1,10 +1,10 @@
-Cypress.Commands.add('loginPage', (username, password)=>{
+Cypress.Commands.add('loginPage', (username, password) => {
     const SAUCEDEMO_PAGE = {
-        USERNAME_TEXTBOX :  '[data-test=username]',
-        PASSWORD_TEXTBOX :'[data-test=password]',
-        LOGIN_BUTTON : '[data-test=login-button]'
+        USERNAME_TEXTBOX: '[data-test=username]',
+        PASSWORD_TEXTBOX: '[data-test=password]',
+        LOGIN_BUTTON: '[data-test=login-button]'
     }
-    
+
     cy.log('-> Type username')
     cy.get(SAUCEDEMO_PAGE.USERNAME_TEXTBOX).type(username)
     cy.log('-> Type the password')
